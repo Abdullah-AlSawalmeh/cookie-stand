ebe204cdefe0853b794b6bee2a3f431c470241b8
Abdullah-AlSawalmeh <abdullahalsawalmeh@gmail.com>
2021-03-07 17:08:32 +0200

    Done 5:08pm

--- js/app.js
+++ js/app.js
@@ -1,99 +1,150 @@
 'use strict';
-
-let counter = 0;
-
-function check(ans) {
-  if (ans === 'yes' | ans ==='y') {
-    // console.log ("Correct !!")
-    alert ('Correct !!');
-    counter = counter + 1;
-
-  } else if (ans ==='no' | ans === 'n') {
-    // console.log ("Wrong!!")
-    alert ('Wrong!!');
-
-
-  } else {
-    // console.log ("Do not put random inputs")
-    alert ('Do not put random inputs');
-
-  }
-
-}
-function q6() {
-  for (let i = 0; i < 4; i++) {
-    let q6 = parseInt( prompt ('Guess the correct number, u have only 4 chanses'));
-    let ans = 6;
-    // console.log(ans);
-    if (q6 < ans) {
-      alert ('too low');
+// alert("sdfds")
+// eslint-disable-next-line no-unused-vars
+let Seattle = {
+  city_name: 'Seattle',
+  min_cust: 23,
+  max_cust: 65,
+  Avg_Cookie: 6.3,
+  working_hors: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
+  result: [],
+  number_of_cutomers_hourly: function () {
+    let number_of_customers = Math.floor(Math.random() * (this.max_cust - this.min_cust) + this.min_cust);
+    // console.log(number_of_customers);
+    return number_of_customers;
+  },
+  simulated_amount_of_cookies: function () {
+    return Math.floor(this.Avg_Cookie*this.number_of_cutomers_hourly());
+  },
+  show_the_result: function () {
+    let element = document.getElementById(this.city_name);
+    for (let i = 0; i < this.working_hors.length; i++) {
+      this.result.push(this.simulated_amount_of_cookies());
+      let tag = document.createElement('li');
+      tag.innerHTML = this.working_hors[i] + ' ' + this.simulated_amount_of_cookies();
+      element.appendChild(tag);
+      console.log(this.working_hors[i] + ' ' + this.simulated_amount_of_cookies());
     }
-    else if (q6 > ans) {
-      alert ('too high');
+  },
+};
+
+let Tokyo = {
+  city_name: 'Tokyo',
+  min_cust: 3,
+  max_cust: 24,
+  Avg_Cookie: 1.2,
+  working_hors: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
+  result: [],
+  number_of_cutomers_hourly: function () {
+    let number_of_customers = Math.floor(Math.random() * (this.max_cust - this.min_cust) + this.min_cust);
+    // console.log(number_of_customers);
+    return number_of_customers;
+  },
+  simulated_amount_of_cookies: function () {
+    return Math.floor(this.Avg_Cookie * this.number_of_cutomers_hourly());
+  },
+  show_the_result: function () {
+    let element = document.getElementById(this.city_name);
+    for (let i = 0; i < this.working_hors.length; i++) {
+      this.result.push(this.simulated_amount_of_cookies());
+      let tag = document.createElement('li');
+      tag.innerHTML = this.working_hors[i] + ' ' + this.simulated_amount_of_cookies();
+      element.appendChild(tag);
+      console.log(this.working_hors[i] + ' ' + this.simulated_amount_of_cookies());
     }
-    else if (q6 === ans) {
-      alert ('correct !!');
-      counter = counter + 1;
-      break; }
-    else {
-      alert ('You put random alphapets');
+  },
+};
+
+
+let Dubai = {
+  city_name: 'Dubai',
+  min_cust: 11,
+  max_cust: 38,
+  Avg_Cookie: 3.7,
+  working_hors: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
+  result: [],
+  number_of_cutomers_hourly: function () {
+    let number_of_customers = Math.floor(Math.random() * (this.max_cust - this.min_cust) + this.min_cust);
+    // console.log(number_of_customers);
+    return number_of_customers;
+  },
+  simulated_amount_of_cookies: function () {
+    return Math.floor(this.Avg_Cookie * this.number_of_cutomers_hourly());
+  },
+  show_the_result: function () {
+    let element = document.getElementById(this.city_name);
+    for (let i = 0; i < this.working_hors.length; i++) {
+      this.result.push(this.simulated_amount_of_cookies());
+      let tag = document.createElement('li');
+      tag.innerHTML = this.working_hors[i] + ' ' + this.simulated_amount_of_cookies();
+      element.appendChild(tag);
+      console.log(this.working_hors[i] + ' ' + this.simulated_amount_of_cookies());
     }
-  }
-}
+  },
+};
 
-function q7(){
-  let cans ;
-  for (let i = 0; i < 6; i++) {
-    let q7 = prompt ('Guess my fav movie, you have only 6 choices');
-    let myFav = ['The Shawshank Redemption','The Godfather","The Godfather: Part II','The Dark Knight','12 Angry Men","Schindler s List','The Lord of the Rings: The Return of the King','Pulp Fiction','The Good, the Bad and the Ugly','The Lord of the Rings: The Fellowship of the Ring' ];
-    for (let i = 0; i < 10; i++) {
-      if (q7 === myFav[i]) {
-        cans = myFav[i];
-        break;
-      }
+
+let Paris = {
+  city_name: 'Paris',
+  min_cust: 20,
+  max_cust: 38,
+  Avg_Cookie: 2.3,
+  working_hors: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
+  result: [],
+  number_of_cutomers_hourly: function () {
+    let number_of_customers = Math.floor(Math.random() * (this.max_cust - this.min_cust) + this.min_cust);
+    // console.log(number_of_customers);
+    return number_of_customers;
+  },
+  simulated_amount_of_cookies: function () {
+    return Math.floor(this.Avg_Cookie * this.number_of_cutomers_hourly());
+  },
+  show_the_result: function () {
+    let element = document.getElementById(this.city_name);
+    for (let i = 0; i < this.working_hors.length; i++) {
+      this.result.push(this.simulated_amount_of_cookies());
+      let tag = document.createElement('li');
+      tag.innerHTML = this.working_hors[i] + ' ' + this.simulated_amount_of_cookies();
+      element.appendChild(tag);
+      console.log(this.working_hors[i] + ' ' + this.simulated_amount_of_cookies());
     }
-    if (q7 === cans) {
-      alert ('Correct');
-      counter = counter + 1 ;
-      break;
-    } else {
-      alert ('Wrong');
+  },
+};
+
+
+let Lima = {
+  city_name: 'Lima',
+  min_cust: 2,
+  max_cust: 16,
+  Avg_Cookie: 4.6,
+  working_hors: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
+  result: [],
+  number_of_cutomers_hourly: function () {
+    let number_of_customers = Math.floor(Math.random() * (this.max_cust - this.min_cust) + this.min_cust);
+    // console.log(number_of_customers);
+    return number_of_customers;
+  },
+  simulated_amount_of_cookies: function () {
+    return Math.floor(this.Avg_Cookie * this.number_of_cutomers_hourly());
+  },
+  show_the_result: function () {
+    let element = document.getElementById(this.city_name);
+    for (let i = 0; i < this.working_hors.length; i++) {
+      this.result.push(this.simulated_amount_of_cookies());
+      let tag = document.createElement('li');
+      tag.innerHTML = this.working_hors[i] + ' ' + this.simulated_amount_of_cookies();
+      element.appendChild(tag);
+      console.log(this.working_hors[i] + ' ' + this.simulated_amount_of_cookies());
     }
-    //    console.log (q7)  ;
-  }
-}
-
-// x = 13 ;
-alert ('Read my Biography first, then answer y/n or yes/no, please, Thank you');
-alert ('Hello there, here is Abdullah AlSawalmeh, I am 26 years old, I have MS.c Degree in Mechatronics engineering and BS.c degree in Mechaninal engineering, I love programming, I have a great knowlegde in Python programming language, my graduation thesis was about how to predict IDC cancer in women using Histopathology images, by applying machine and deep learning approaches, Also I worked in Hungary for 3 months as a customer support agent for Ryanair airlines, My goal of taking this course is to develop my skills in programming, getting a suitable job and having a new nice friends, Thank you');
-let q1 = prompt ('1st question: Do I have a great knowledge in Machine and deep learning? (Python) ? ').toLowerCase();
-check (q1);
-let q2 = prompt ('2nd question: Is my name Abdullah AlSawalmeh?').toLowerCase();
-check (q2);
-let q3 = prompt ('3rd question: Is the company name that I worked in, called Ryanair airlines?').toLowerCase();
-check (q3);
-let q4 = prompt ('4th question: Do I have two scientific degrees?').toLowerCase();
-check (q4);
-let q5 = prompt ('5th question: What my MS.c thesis was about? is it was about predicting IDC cancer in women using Histopathology images, by applying machine and deep learning approaches ? ').toLowerCase();
-check (q5);
+  },
+};
 
 
-q6();
-
-q7();
-
-
-
-
-alert ('Your score is: ' + counter);
-
-
-// // 2nd Story
-// let qNmae = prompt ("And you, What is your name? ");
-// alert ("Hello, " + qNmae + " It is nice to meet you !!")
-// alert (qNmae + ", I hope, you live a long happy life <3, Thank you")
-
-
-
-
+// Seattle.number_of_cutomers_hourly();
+Seattle.show_the_result();
+Tokyo.show_the_result();
+Dubai.show_the_result();
+Paris.show_the_result();
+Lima.show_the_result();
+// console.log(Seattle.result);
 
